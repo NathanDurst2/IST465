82369a3d5594aa959758ad0c407a6c1cc3f8d3b6
Nathan Durst <2742087@vikes.csuohio.edu>
2020-02-21 23:25:34 -0500

    test

--- ERP/Class1.cs
+++ ERP/Class1.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ERP
-{//test
+{
     public class SqliteDataAccess
     {
         public static List<Customer> LoadAllCustomer()
